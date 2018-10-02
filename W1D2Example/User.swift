b3ac3183fced6b5cbc86f86dd9a54a9c619b20e6
Christopher Dean <chrisjgdean@gmail.com>
2018-10-02 02:10:42 -0700

    first commit

---
+++ W1D2Example/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  W1D2Example
+//
+//  Created by Chris Dean on 2018-09-26.
+//  Copyright © 2018 Chris Dean. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+
+}
